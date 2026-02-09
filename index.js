f77e64a50a470ba7942cf8dcb0a4cb5eeabc0847
maklevia <maria.klevchyk@gmail.com>
2026-02-09 19:19:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,208 @@
+//TO FIX: more than one  click on start button won't allow to stop the game
+
+
+let gridSize = 10;
+let grid = [];
+
+let playbackTimeout = null;
+
+function checkAliveConditionForCell(row, col, grid){
+    let aliveNeighborsCounter = 0;
+    for (let colIndex = -1; colIndex < 2; colIndex++) {
+        for (let rowIndex = -1; rowIndex < 2; rowIndex++) {
+            if (rowIndex === 0 && colIndex === 0) {
+                continue;
+            }
+            if ((row + rowIndex < 0) || (row + rowIndex > grid.length - 1)) {
+                continue;
+            }
+            if ((col + colIndex < 0) || (col + colIndex > grid.length - 1)) {
+                continue;
+            }
+            if (grid[row + rowIndex][col + colIndex] === true) {
+                aliveNeighborsCounter++;
+            }
+        }
+    }
+
+    if (grid[row][col] === false) {
+        if (aliveNeighborsCounter === 3) {
+            return true;
+        }
+
+        return false;
+    }
+    else {
+        if (aliveNeighborsCounter < 2) {
+            return false;
+        }
+        if (aliveNeighborsCounter > 3) {
+            return false;
+        }
+        return true;
+    }
+}
+
+function iterateGridAndCheckConditionForCells(){
+    const copyGrid = structuredClone(grid);
+    for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+        for (let colIndex = 0; colIndex < grid[rowIndex].length; colIndex++) {
+            copyGrid[rowIndex][colIndex] = checkAliveConditionForCell(rowIndex, colIndex, grid);
+        }
+    }
+
+    grid = copyGrid;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    insertGridSize()
+    createGrid();
+    addListenersToCells();
+    increaseGridSize();
+    decreaseGridSize();
+    resetCells();
+    randomizeGrid();
+    listenStartButton();
+    listenStopButton();
+})
+
+function listenStartButton() {
+    let button = document.querySelector('#start');
+    button.addEventListener('click', () => {
+        clearInterval(playbackTimeout);
+        playbackTimeout = setInterval(() => {
+            iterateGridAndCheckConditionForCells();
+            clearWrapper();
+            createGrid(0, true);
+            addListenersToCells();
+        }, 300)
+    })
+}
+
+function listenStopButton() {
+    let button = document.querySelector('#stop');
+    button.addEventListener('click', () => {
+        clearInterval(playbackTimeout);
+    })
+}
+
+function resetCells() {
+    const resetButton = document.querySelector('#reset');
+    resetButton.addEventListener('click', () => {
+        clearWrapper();
+        createGrid();
+        addListenersToCells();
+        initializeGrid();
+    })
+}
+
+function clearWrapper() {
+    const wrapper = document.querySelector('.wrapper');
+    wrapper.innerHTML = '';
+}
+
+function increaseGridSize() {
+    const button = document.querySelector('#increase');
+    button.addEventListener('click', () => {
+        gridSize++;
+        clearWrapper();
+        createGrid();
+        addListenersToCells();
+        insertGridSize()
+    })
+}
+
+function decreaseGridSize() {
+    const button = document.querySelector('#decrease');
+    button.addEventListener('click', () => {
+        gridSize--;
+        clearWrapper();
+        createGrid();
+        addListenersToCells();
+        insertGridSize()
+    })
+}
+
+function insertGridSize() {
+    const sizeCounter = document.querySelector('.sizeCounter');
+    sizeCounter.textContent = 'Grid size: ' + gridSize;
+}
+
+function randomizeGrid() {
+    const button = document.querySelector('#random');
+    button.addEventListener('click', () => {
+        clearWrapper();
+        createGrid(0.35);
+        addListenersToCells();
+    })
+}
+
+function initializeGrid(randomizeCoef) {
+    grid = [];
+    for (let i = 0; i < gridSize; i++) {
+        let row = [];
+        for (let j = 0; j < gridSize; j++) {
+            if (randomizeCoef && randomizeCoef > Math.random()) {
+                row.push(true);
+            }
+            else {
+                row.push(false);
+            }
+        }
+        grid.push(row);
+    }
+}
+function createGrid(randomizeCoef, skipInitialization) {
+    if(!skipInitialization){
+        initializeGrid(randomizeCoef)
+    }
+
+    const wrapper = document.querySelector('.wrapper');
+    for (let i = 0; i < gridSize; i++) {
+        const newRow = document.createElement('div')
+        newRow.classList.add('row')
+        for (let j = 0; j < gridSize; j++) {
+            const newCell = document.createElement('div')
+            newCell.classList.add('cell')
+            newCell.id = i + ' - ' + j;
+            if (grid[i][j]) {
+                newCell.classList.add('alive')
+            }
+            else {
+                newCell.classList.add('dead');
+            }
+            newRow.appendChild(newCell)
+        }
+        wrapper.appendChild(newRow);
+    }
+}
+
+function addListenersToCells() {
+    const cellsElements = document.getElementsByClassName('cell');
+    const cells = Array.from(cellsElements);
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+            let rowId = cell.id.split(' - ')[0];
+            let colId = cell.id.split(' - ')[1];
+            if (cell.classList.value.includes('dead')) {
+                grid[rowId][colId] = true;
+                cell.classList.remove('dead');
+                cell.classList.add('alive');
+            }
+            else if (cell.classList.value.includes('alive')) {
+                grid[rowId][colId] = false;
+                cell.classList.remove('alive');
+                cell.classList.add('dead');
+            }
+        })
+    })
+}
+
+
+
+
+
+
+
+
